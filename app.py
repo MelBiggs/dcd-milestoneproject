b15a838632458256919f0defa2bf6852457417f5
MelBiggs <melissabiggs16@gmail.com>
2019-12-23 11:39:24 +0000

    Added requirements.txt file

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
-#MONGODB_NAME = os.environ.get('MONGODB_NAME')
+# MONGODB_NAME = os.environ.get('MONGODB_NAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
 
 @app.route('/')
 def hello_world():
     return "Hello World"
+
 
 # execute app__init__.py
 if __name__ == "__main__":
